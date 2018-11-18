ad079dd396c8c841aca973fd353445d2825817a9
Jody <smile.jody@gmail.com>
2018-11-18 15:53:35 -0500

    Add default and user nav components

---
+++ wlust360/src/components/NavbarAuth.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+   div
+      div.container
+         nav.navbar.navbar-expand-lg.navbar-dark.bg-primary.fixed-top
+            a.navbar-brand(href='/') WLUST 360
+            p Welcome {{ username }}
+            button.navbar-toggler(type='button' data-toggle='collapse' data-target='#navbarCollapse' aria-controls='navbarCollapse' aria-expanded='false' aria-label='Toggle Navigation'): span(class='navbar-toggler-icon')
+
+            .collapse.navbar-collapse.justify-content-end(id='navbarCollapse')
+               ul.navbar-nav.text-right
+                  li.nav-item.active
+                     a.nav-link(href='#') Trips
+                  li.nav-item.active
+                     a.nav-link(href='#') Profile
+                  li.nav-item(v-if="user")
+                     a.btn.btn-secondary(href='#') Logout
+                  li.nav-item(v-else)
+                     a.btn.btn-secondary(href='#') Login/SignUp
+</template>
+
+<script>
+
+export default {
+  name: 'NavbarAuth',
+  props: ['username']
+}
+</script>
+
