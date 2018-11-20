e79e2f8e715b0bb7698f180767be0d081ca693d5
Jody <smile.jody@gmail.com>
2018-11-20 01:49:50 -0500

    Token middleware

---
+++ src/middlewares/token.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+let secret;
+if (process.env.NODE_ENV === 'production') {
+   secret = process.env.secret;
+} else {
+   const config = require('./../config/config');
+   secret = config.secret;
+}
+
+//middleware to handle web token checking
+const validateToken = (req, res, next) => {
+   const token = req.headers['x-access-token'];
+   if (token) {
+      jwt.verify(token, secret, (err, decoded) => {
+         if(err) {
+            return res.status(500).json({ auth: false, message: "Auth failed"});
+         }
+      
+         req.decoded = decoded;
+         next();
+      });
+   } else {
+      return res.status(403).json({ auth: false, message: "No Access Token Found"});
+   }
+}
+
+module.exports = { validateToken };
