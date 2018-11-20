208858ac94576d07c136ac63fbce039af0fd6063
Jody <smile.jody@gmail.com>
2018-11-20 03:41:52 -0500

    Add lint and indent comments

--- wlust360/src/config/apiUrls.js
+++ wlust360/src/config/apiUrls.js
@@ -1,10 +1,11 @@
 /* eslint-disable semi */
 const url = {};
-/*url.signup = 'http://127.0.0.1:3000/signup';
-url.login = 'http://127.0.0.1:3000/login';
-url.me = 'http://127.0.0.1:3000/me';
-url.createTrip = 'http://127.0.0.1:3000/api/trip/create-trip';
-url.getTrip = 'http://127.0.0.1:3000/api/trip/my-trips';
+/*
+   url.signup = 'http://127.0.0.1:3000/signup';
+   url.login = 'http://127.0.0.1:3000/login';
+   url.me = 'http://127.0.0.1:3000/me';
+   url.createTrip = 'http://127.0.0.1:3000/api/trip/create-trip';
+   url.getTrip = 'http://127.0.0.1:3000/api/trip/my-trips';
 */
 url.signup = 'https://wlust-360.herokuapp.com/signup';
 url.login = 'https://wlust-360.herokuapp.com/login';
