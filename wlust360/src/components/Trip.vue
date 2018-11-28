307d9a53760fe70c427a946c3cb4717a2481323a
Jody <smile.jody@gmail.com>
2018-11-28 18:42:00 -0500

    Change card styles

--- wlust360/src/components/Trip.vue
+++ wlust360/src/components/Trip.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
-   .card-text-white.bg-success.mb-3
-      .card-header {{ trip.title }}
+   .card-text-white.bg-success.mb-3.trip-card
+      .card-header.trip-card-title {{ trip.title }}
       .card-body
          h2.title {{ trip.dest }}
          h2.title {{ trip.start }} - {{ trip.end }}
-         h2.card-text Add some events to your trip 
+         button.btn.btn-warning Add some events to your trip 
 </template>
 
 <script>
