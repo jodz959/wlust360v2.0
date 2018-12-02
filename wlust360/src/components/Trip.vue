87af969b38b7daac582b31067feb50c4333d8547
Jody <smile.jody@gmail.com>
2018-12-02 04:38:12 -0500

    Card is clickable

--- wlust360/src/components/Trip.vue
+++ wlust360/src/components/Trip.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
-   .card-text-white.bg-success.mb-3.trip-card
-      .card-header.trip-card-title {{ trip.title }}
-      .card-body
-         h2.title {{ trip.dest }}
-         h2.title 
-            span {{ trip.start | moment("dddd, MMMM do YYYY") }} - 
-            span {{ trip.end | moment("dddd, MMMM do YYYY") }}
-         router-link(:to="{ name: 'TripSummary', params: { trip: trip.slug.slice(0, -22) } }")
+   router-link(:to="{ name: 'TripSummary', params: { trip: trip.slug } }")
+      .card-text-white.bg-success.mb-3.trip-card(@click="redirect(trip)")
+         .card-header.trip-card-title {{ trip.title }}
+         .card-body
+            h2.title {{ trip.dest }}
+            h2.title 
+               span {{ trip.start | moment("dddd, MMMM do YYYY") }} - 
+               span {{ trip.end | moment("dddd, MMMM do YYYY") }}
             button.btn.btn-warning View Trip 
 </template>
 
