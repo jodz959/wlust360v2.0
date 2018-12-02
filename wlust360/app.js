cde38afdb0539ccd272b771c937cc70338546241
Jody <smile.jody@gmail.com>
2018-12-02 03:29:26 -0500

    Add iou and plan api paths to express app

--- wlust360/app.js
+++ wlust360/app.js
@@ -9,6 +9,8 @@ const app = express();
 
 const authRoutes = require('./app_src/routes/auth');
 const tripRoutes = require('./app_src/routes/trip');
+const iouRoutes = require('./app_src/routes/ious');
+const planRoutes = require('./app_src/routes/plan');
 
 let dbURL;
 let secret;
@@ -52,6 +54,8 @@ app.use((req, res, next) => {
 //routes
 app.use('/auth', authRoutes);
 app.use('/api/trip', tripRoutes);
+app.use('/api/iou', iouRoutes);
+app.use('/api/plan', planRoutes);
 
 //catch any routes not registeres
 app.get('*', (req, res) => {
