7dd548a51a16f023fbaa5bd1d67c02938deb7cf0
Jody <smile.jody@gmail.com>
2018-12-02 04:44:46 -0500

    Add components for IOUs and local info

---
+++ wlust360/src/components/IOUCard.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+   .card-text-white.bg-success.mb-3.trip-card
+      .card-header.trip-card-title 
+         span {{ iou.date | moment("dddd, MMMM do YYYY") }}
+      .card-body
+         h2.title {{ iou.title }}
+         h3.title Total - ${{ iou.total }}
+         p.card-text
+            | {{ iou.notes }}
+      
+</template>
+
+<script>
+export default {
+  name: 'Trip',
+  props: {
+   iou: {
+     type: Object,
+     default: function () {}
+    }
+  }
+}
+</script>
---
+++ wlust360/src/components/LocalCard.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+   .card-text-white.mb-3.trip-card(:class="'bg-' + local.color")
+      .card-header.trip-card-title.local-card-header {{ local.title }}
+      .card-body.local-card-body
+         img(:src="getImageUrl(local.imgSrc)")
+         h2.title.local-card-number {{ local.number }}
+         p.card-text
+            | {{ local.text }}
+</template>
+<script>
+
+export default {
+  name: 'LocalCard',
+  props: {
+   local: {
+     type: Object,
+     default: function () {}
+    }
+  },
+  methods: {
+    getImageUrl(url) {
+       return require(""+url)
+    }
+  }
+}
+</script>
+
